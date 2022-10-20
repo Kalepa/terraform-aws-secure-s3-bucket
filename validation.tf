8a1e8bcd721b668f56657a8d807fc22fdb640991
Maciej Wilczyński <maciej@lupine.software>
2022-10-20 22:31:06 +0200

    Update dependencies

--- validation.tf
+++ validation.tf
@@ -1,20 +1,20 @@
 module "assert_single_kms_key" {
-  source        = "Invicton-Labs/assertion/null"
-  version       = "~>0.2.1"
+  source        = "Kalepa/assertion/null"
+  version       = "~> 0.2"
   condition     = !(local.create_new_kms_key == true && local.create_replica_kms_key == true)
   error_message = "If `create_new_kms_key` is `true`, `create_replica_kms_key` cannot be `true`."
 }
 
 module "assert_no_existing_kms_if_new" {
-  source        = "Invicton-Labs/assertion/null"
-  version       = "~>0.2.1"
+  source        = "Kalepa/assertion/null"
+  version       = "~> 0.2"
   condition     = !(local.create_new_kms_key == true && var.kms_key_arn != null)
   error_message = "If `create_new_kms_key` is `true`, `kms_key_arn` must be `null`."
 }
 
 module "assert_source_kms_key" {
-  source  = "Invicton-Labs/assertion/null"
-  version = "~>0.2.1"
+  source  = "Kalepa/assertion/null"
+  version = "~> 0.2"
   depends_on = [
     module.assert_single_kms_key,
     module.assert_no_existing_kms_if_new,
